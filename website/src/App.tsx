fc4e6f5747685857541b928271e412a326936d93
Ken <ken.aleksander@gmail.com>
2021-04-29 19:00:43 +0200

    satt opp CRA + workspace

---
+++ website/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app.css";
+import {ContentContainer} from "@navikt/ds-react";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="content"></div>
+    </div>
+  );
+};
+
+export default App;
