adfa77f95e6b9a7a4430da536740d0b345eb23db
Ken <ken.aleksander@gmail.com>
2021-08-29 12:51:15 +0200

    oppdatert kode

--- tracker/src/gitCommands.ts
+++ tracker/src/gitCommands.ts
@@ -2,6 +2,22 @@ const fs = require("fs");
 const path = require("path");
 const NodeGit = require("nodegit");
 
+const gitAuth = {
+  fetchOpts: {
+    callbacks: {
+      certificateCheck: function () {
+        return 1;
+      },
+      credentials: function () {
+        return NodeGit.Cred.userpassPlaintextNew(
+          process.env.TOKEN,
+          "x-oauth-basic"
+        );
+      },
+    },
+  },
+};
+
 const CloneOrPull = (name: string) =>
   new Promise((resolve, reject) => {
     const repoPath = path.join(process.cwd(), `repos/${name}`);
@@ -9,21 +25,7 @@ const CloneOrPull = (name: string) =>
     const cloneUrl = `https://${process.env.TOKEN}:x-oauth-basic@github.com/navikt/${name}`;
     fs.access(repoPath, function (err: Error) {
       if (err) {
-        NodeGit.Clone(`cloneUrl`, repoPath, {
-          fetchOpts: {
-            callbacks: {
-              certificateCheck: function () {
-                return 1;
-              },
-              credentials: function () {
-                return NodeGit.Cred.userpassPlaintextNew(
-                  process.env.TOKEN,
-                  "x-oauth-basic"
-                );
-              },
-            },
-          },
-        })
+        NodeGit.Clone(`cloneUrl`, repoPath, gitAuth)
           .then(function (repo: any) {
             // We don't want to directly pass `callback` because then the consumer gets a copy
             // of the repository object, which is public API
@@ -35,7 +37,7 @@ const CloneOrPull = (name: string) =>
 
         NodeGit.Repository.open(repoPath)
           .then(function (repository: any) {
-            repository.fetchAll();
+            repository.fetchAll(gitAuth);
           })
           .then(function () {
             resolve(null);
