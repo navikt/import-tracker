c672b566bec2f427fafab83ae39dba7bc22c1ef1
Ken <ken.aleksander@gmail.com>
2021-08-31 14:14:05 +0200

    r21

--- .github/workflows/comment/index.js
+++ .github/workflows/comment/index.js
@@ -17,7 +17,7 @@ async function run() {
 
     try {
       execSync("git status");
-      execSync(`git fetch --depth 1 origin ${ref}`);
+      execSync(`git fetch origin ${ref}`);
       execSync("git status");
       output = execSync(`yarn lerna version patch --no-push`, { input: "n" });
     } catch (error) {
