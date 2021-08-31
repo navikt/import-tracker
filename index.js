6039fcb024d7f12c67dd9325ed23720fa2fe34d2
Ken <ken.aleksander@gmail.com>
2021-08-31 12:37:56 +0200

    la til txt filer

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const readline = require("readline");
+
+async function processLineByLine() {
+  const fileStream = fs.createReadStream("out.txt");
+
+  const changes = [];
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  let isChange = false;
+  for await (const line of rl) {
+    if (line.includes("Changes:")) {
+      isChange = true;
+    }
+    if (isChange && line.startsWith(" - ")) {
+      changes.push(line);
+    } else {
+      isChange = true;
+    }
+  }
+  console.log(changes);
+}
+
+processLineByLine();
