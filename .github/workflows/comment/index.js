3a7c4e213483cd796a23ddfa48c8b925fbe670cf
Ken <ken.aleksander@gmail.com>
2021-08-31 15:06:10 +0200

    delete old comment

--- .github/workflows/comment/index.js
+++ .github/workflows/comment/index.js
@@ -15,14 +15,29 @@ async function run() {
     const ref = github.context.sha;
 
     let output;
-
+    if (github.context.payload.pull_request.title)
+      const title = github.context.payload.pull_request.title;
+    let version = "";
+    switch (true) {
+      case title.includes("[fix]"):
+        version = "patch";
+        break;
+      case title.includes("[feature]"):
+        version = "minor";
+        break;
+      case title.includes("[breaking]"):
+        version = "major";
+        break;
+      default:
+        return;
+    }
     try {
       execSync("git checkout main");
 
       execSync(`git fetch  origin ${ref}`);
 
       execSync("ls");
-      output = execSync(`yarn lerna version patch --no-push`, {
+      output = execSync(`yarn lerna version ${version} --no-push`, {
         input: "n",
       });
     } catch (error) {
@@ -44,6 +59,25 @@ async function run() {
     changes.forEach((x) => {
       prText = `${prText} - ${x}\n`;
     });
+
+    const comments = await octokit.rest.issues.listComments({
+      ...github.context.repo,
+      issue_number: github.context.payload.pull_request.number,
+    });
+
+    let commentId = "";
+    comments.forEach((x) => {
+      if (x.user.login === "github-actions[bot]") {
+        commentId = x.id;
+      }
+    });
+
+    if (commentId !== "") {
+      await octokit.rest.issues.deleteComment({
+        ...github.context.repo,
+        comment_id: commentId,
+      });
+    }
 
     await octokit.rest.issues.createComment({
       ...github.context.repo,
